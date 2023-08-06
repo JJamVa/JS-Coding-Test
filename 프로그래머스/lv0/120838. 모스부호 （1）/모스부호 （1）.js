c2330a7b5ee91bb985d16deef4330f3b598be407
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-07 03:38:13 +0900

    [level 0] Title: 모스부호 (1), Time: 0.18 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,13 @@
+function solution(letter) {
+    var answer = ''
+    var morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    var arr = letter.split(" ")
+    for(var idx in arr) answer += morse[arr[idx]]
+    return answer;
+}
