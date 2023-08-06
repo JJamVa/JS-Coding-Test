04e5dfc89f99c4eddf64c3a27bd469ed6fc69c66
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-07 03:44:54 +0900

    [level 0] Title: 옹알이 (1), Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,16 +1,3 @@
 function solution(babbling) {
-  let answer = 0;
-
-  for (let i = 0; i < babbling.length; i++) {
-    if (babbling[i].includes('aya')) babbling[i] = babbling[i].replace("aya"," ")
-    if (babbling[i].includes('ye')) babbling[i] = babbling[i].replace("ye"," ")
-    if (babbling[i].includes('woo')) babbling[i] = babbling[i].replace("woo"," ")
-    if (babbling[i].includes('ma')) babbling[i] = babbling[i].replace("ma"," ")
-  }
-
-  for(var j = 0; j < babbling.length; j++){
-      if(babbling[j].trim() === "") answer++;
-  }
-    
-  return answer;
+   return babbling.map(e => e.replace(/aya|ye|woo|ma/g, "")).filter(v => (v==="")).length;
 }
