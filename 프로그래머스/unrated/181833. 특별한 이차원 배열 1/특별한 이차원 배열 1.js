fddeb896ca44cafe5d3bea0cade85b32d42ed685
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-07 03:59:38 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.26 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    var answer = Array.from(Array(n), () => Array(n).fill(0));
+    for(var i = 0; i < answer.length; i++){
+        answer[i][i] += 1   
+    }
+    return answer;
+}
