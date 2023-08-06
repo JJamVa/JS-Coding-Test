dd4c65756d50381c75de4cfec5a5abc5b0681d02
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-07 03:34:09 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 48.19 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    for(var i = 1; i <= Number(input[0]); i++){
+        var arr = Array.from({length:i}, element => "*")
+        console.log(`${arr.join("")}`)
+    };
+});
