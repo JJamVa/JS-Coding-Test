ecef0e49c36c1cad9a4f2708bfcaa662312891b4
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-07 18:03:06 +0900

    [level 0] Title: 7의 개수, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,7 +1,6 @@
 function solution(array) {
     var answer = array.join("")
     var arr_7 = Array.from(String(answer))
-    console.log(arr_7)
     var check = 0
     for(var idx in arr_7){
         if(arr_7[idx] === String(7)) check++
