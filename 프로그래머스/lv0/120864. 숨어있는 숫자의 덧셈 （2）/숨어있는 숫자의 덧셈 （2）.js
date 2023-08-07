6a02e6e1175d81d5c1df4d2e0d744b882d717843
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-07 17:19:50 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,7 +1,6 @@
 function solution(my_string) {
     return my_string
             .replace(/[a-z]|[A-Z]/g, " ")
-            .trim()
             .split(" ")
             .filter(e => e !== "")
             .map(Number)
