c6259c69bebe35ae26d0c70b0691942f029bdda6
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-10 19:05:45 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,12 +1,13 @@
 function solution(my_string) {
     var answer = [];
-    var str_arr = my_string.split(" ")
-    for(var i = 0; i < str_arr.length; i++){
-        if(isNaN(str_arr[i])){
-            str_arr[i + 1] = str_arr[i] + str_arr[i + 1]
-            continue
+    var check = false
+    var arr = my_string.split(" ")
+    arr.forEach(e => {
+        if(e === "-") check = true
+        if(!isNaN(e)) {
+            check ? answer.push("-" + e) : answer.push(e)
+            check = false
         }
-        answer.push(str_arr[i])
-    }
+    })
     return answer.map(Number).reduce((a,b) => a + b,0);
 }
