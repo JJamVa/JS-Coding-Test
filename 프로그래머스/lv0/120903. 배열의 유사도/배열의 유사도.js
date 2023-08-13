0be8cd146a1faa0f1ce979f0edf1e4cc8d47ff06
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-14 01:49:11 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,6 +1,3 @@
 function solution(s1, s2) {
-    var answer = s1.concat(s2)
-    const set = new Set()
-    answer.map(element => set.add(element))
-    return Math.abs(set.size - (s1.length+s2.length));
+    return s1.filter((e,i) => s2.includes(e)).length;
 }
