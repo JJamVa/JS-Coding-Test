39a570a53d7271179211bd359eedb2ec43a03527
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-19 07:35:18 +0900

    [level 0] Title: 다항식 더하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -4,16 +4,14 @@ function solution(polynomial) {
     var [x_arr,num_arr] = [[],[]]
     var arr = polynomial.split(" ")
     for(var i in arr){
-        if(arr[i].includes("x") === true){
-            if(arr[i].length === 1){
-                x_arr.push(1)
-            }
+        if(arr[i].includes("x")){
+            if(arr[i].length === 1) x_arr.push(1)
             else{
                 var arr1 = arr[i].split("x")
                 x_arr.push(parseInt(arr1[0]))
             }
         }
-        else if(isNaN(parseInt(arr[i])) === false){
+        else if(!isNaN(parseInt(arr[i]))){
             num_arr.push(parseInt(arr[i]))
         }
     }
@@ -21,15 +19,9 @@ function solution(polynomial) {
     if(x_arr.length > 0){
         const x_sum = x_arr.reduce((a,b) => a+b)
         if(num_arr.length > 0){
-            if(x_sum === 1) {
-                answer += ("x + ")
-            }
-            else{
-                answer += (x_sum.toString() + "x + ")
-            }
-        }else{
-            answer += (x_sum.toString() + "x")
-        }
+            if(x_sum === 1) answer += ("x + ")
+            else answer += (x_sum.toString() + "x + ")
+        }else answer += (x_sum.toString() + "x")
     }
     
     if(num_arr.length > 0){
