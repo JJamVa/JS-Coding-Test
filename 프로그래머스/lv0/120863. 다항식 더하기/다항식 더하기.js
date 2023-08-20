8bb5d8e2d7ee767fc5bde353224e03037afd76de
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-20 15:55:26 +0900

    [level 0] Title: 다항식 더하기, Time: 0.19 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -5,13 +5,10 @@ function solution(polynomial) {
         if(answer[x].includes("x")) answer[x] !== "x" ? x_arr.push(answer[x]) : x_arr.push("1x")
         else num_arr.push(answer[x])
     }
-    let x_result = x_arr.join("").split("x").filter(e => e !== "").reduce((a,b) => a + parseInt(b),0)
+    let x_result = x_arr.map(e => e.replace("x","")).reduce((a,b) => a + parseInt(b),0)
     let num_result = num_arr.reduce((a,b) => a + parseInt(b),0);
     
-    let add = ""
-    if(x_result === 0) add = num_result.toString()
-    else if(num_result === 0) add = `${x_result !== 1 ? x_result : ""}x`
-    else add = `${x_result !== 1 ? x_result : ""}x + ${num_result.toString()}`
-    
-    return add;
+    if(x_result === 0) return num_result.toString()
+    else if(num_result === 0) return `${x_result !== 1 ? x_result : ""}x`
+    return `${x_result !== 1 ? x_result : ""}x + ${num_result}`
 }
