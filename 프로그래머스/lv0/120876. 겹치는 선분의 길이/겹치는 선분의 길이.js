5269b08d1f7200601197ede6d77c2830fa728a7e
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-26 16:29:41 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,16 +1,16 @@
 function solution(lines) {
     var answer = [...lines].sort((a,b) => a[0] - b[0]).flat();
     let split_arr = []
-    let arr = []
+    let result = 0
     for(var i = answer[0] - 1; i < answer[answer.length - 1]; i++){
         split_arr.push([i,i+1])
     }
     for(var i = 0; i < split_arr.length; i++){
         let count = 0
-        if(lines[0][0] <= split_arr[i][0] && split_arr[i][1] <= lines[0][1]) count++;
-        if(lines[1][0] <= split_arr[i][0] && split_arr[i][1] <= lines[1][1]) count++;
-        if(lines[2][0] <= split_arr[i][0] && split_arr[i][1] <= lines[2][1]) count++;
-        if(count > 1) arr.push(split_arr[i])
+        for(var j = 0; j < lines.length; j++){
+            if(lines[j][0] <= split_arr[i][0] && split_arr[i][1] <= lines[j][1]) count++;
+        }
+        if(count > 1) result++
     }
-    return arr.length
+    return result
 }
