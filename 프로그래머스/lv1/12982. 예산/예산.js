2a4e990792aed5775bbe78467156e810239b5ffd
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-28 02:57:55 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,13 +1,8 @@
 function solution(d, budget) {
     var answer = [...d];
-    d = d.sort((a,b) => a - b)
-    while(d.length !== 0 || budget >= 0){
-        let current = d.shift()
-        budget -= current
-        if(budget < 0){
-            d.unshift(current)
-            break
-        }
+    while(budget > 0){
+        if(budget < d[0])break
+        budget -= d.shift()
     }
     return answer.length - d.length;
 }
