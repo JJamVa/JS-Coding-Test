c5f95d16063b6865eedafb1c4228135f63799872
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-28 16:41:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,9 +1,9 @@
 function solution(num) {
     var answer = 0;
-    let count = 0
+    let count = 1
     while(num !== 1 && count < 500){
         num = num % 2 === 0 ? parseInt(num / 2) : num * 3 + 1
         count++
     }
-    return count === 500 ? -1 : count;
+    return count === 500 ? -1 : count - 1;
 }
