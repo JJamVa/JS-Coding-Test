aac2c376ad4a610cdb53365105ca9fc6ffb82336
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-08-30 00:11:06 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,10 +1,15 @@
 function solution(emergency) {
-    var count = Array.from({length:emergency.length}).fill(0)
-    emergency.filter((e,i) => {
-        for(var j of emergency){
-            if(e <= j) count[i]++
-        }
-    })
-
-    return count;
+    let newEmergency = [];
+    for(let i=0; i<emergency.length; i++){
+        newEmergency.push({value: emergency[i], index: i})
+    }
+    newEmergency.sort((a,b) => b.value-a.value)
+    var answer = new Array(emergency.length);
+    
+    for(let j=0; j<newEmergency.length; j++){
+        let item = newEmergency[j];
+        answer[item.index] = j + 1;
+    }
+    
+    return answer;
 }
