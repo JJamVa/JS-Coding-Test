e09f1cfd465930f3e19bc63f202782149d55fe64
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-09-02 17:41:58 +0900

    [level 1] Title: 시저 암호, Time: 1.99 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -3,9 +3,12 @@ function solution(s, n) {
         let code = e.charCodeAt(0) + n % 26
         if(e === " ") return " ";
         else if(e === e.toLowerCase()){
-            if(code > 122) code = 96 + (code % 122)
+            if(code > "z".charCodeAt(0)){
+                code = "a".charCodeAt(0) - 1 + (code % 'z'.charCodeAt(0))
+            }
         }else{
-            if(code > 90) code = 64 + (code % 90)
+            if(code > "Z".charCodeAt(0))
+                code = "A".charCodeAt(0) - 1 + (code % 'Z'.charCodeAt(0))
         }
         return String.fromCharCode(code)
     });
