8ca58365558321700fa833ebd49b60889a3c38eb
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-09-06 23:13:01 +0900

    [level 0] Title: 저주의 숫자 3, Time: 3.50 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -2,6 +2,7 @@ function solution(n) {
     let value = 1
     for(let count = 1; count <= n; count++,value++){
         while(value.toString().includes("3") || value % 3 === 0) value++;
+        console.log(count,value)
     }
     return value -1;
 }
