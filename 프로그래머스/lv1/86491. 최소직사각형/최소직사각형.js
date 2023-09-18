fb90b9e7bf0c985cdae0dd64b5611f8ccf26e501
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-09-18 17:41:18 +0900

    [level 1] Title: 최소직사각형, Time: 24.62 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,7 +1,7 @@
 function solution(sizes) {
     let orderby_arr = [...sizes].map(e => e.sort((a,b) => a - b))
-    let row_arr = [...orderby_arr].flat().filter((e,i) => i % 2 === 0).sort((a,b) => b - a)
-    let col_arr = [...orderby_arr].flat().filter((e,i) => i % 2 === 1).sort((a,b) => b - a)
+    let row_arr = [...orderby_arr].flat().filter((e,i) => i % 2 === 0)
+    let col_arr = [...orderby_arr].flat().filter((e,i) => i % 2 === 1)
 
-    return row_arr[0] * col_arr[0];
+    return Math.max(...row_arr) * Math.max(...col_arr);
 }
