ee9dcb08fd5100424c9b90f5c5cdd3e9013eb7aa
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-09-27 17:14:22 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 14.56 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -4,7 +4,6 @@ function solution(s) {
     for(var i = 0; i < s.length; i++){
        !check_str.includes(s[i]) ? answer.push(-1) : answer.push(i - check_str.lastIndexOf(s[i]))
         check_str += s[i]
-        s = s.replace(s[i]," ")
     }
     
     return answer
