f2df61223c360a4183c148cd56cf410efe100205
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-09-30 18:09:52 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,30 +1,20 @@
+let check = arr => {
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i - 1] > 1 && arr[i] === -1){
+            arr[i - 1] -= 1
+            arr[i] += 1
+        }else if(arr[i - 1] === -1 && arr[i] > 1){
+            arr[i - 1] += 1
+            arr[i] -= 1
+        }
+    }
+    return arr   
+}
+
 function solution(n, lost, reserve) {
     let answer = Array.from({length:n}, (e,i) => reserve.includes(i+1) ? 2 : 0);
     for(let i = 0; i < lost.length; i++){
         answer[lost[i] - 1] === 0 ? answer[lost[i] - 1] = -1 : answer[lost[i] - 1] = 1
     }
-    
-    for(let j = 1; j < answer.length; j++){
-        if(answer[j - 1] > 1 && answer[j] === -1){
-            answer[j - 1] -= 1
-            answer[j] += 1
-        }else if(answer[j - 1] === -1 && answer[j] > 1){
-            answer[j - 1] += 1
-            answer[j] -= 1
-        }
-    }
-    
-    answer.reverse()
-    
-    for(let k = 1; k < answer.length; k++){
-        if(answer[k - 1] > 1 && answer[k] === -1){
-            answer[k - 1] -= 1
-            answer[k] += 1
-        }else if(answer[k - 1] === -1 && answer[k] > 1){
-            answer[k - 1] += 1
-            answer[k] -= 1
-        }
-    }
-
-    return answer.filter(e => e >= 0).length;
+    return check(check(answer).reverse()).filter(e => e >= 0).length;
 }
