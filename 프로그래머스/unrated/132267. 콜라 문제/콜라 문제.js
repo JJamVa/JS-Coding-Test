7e02f4fb94766c7a64b6a93cd3c3d689c8b14b68
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-10-04 19:21:48 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,4 +1,10 @@
 function solution(a, b, n) {
-    var answer = 0;
-    return answer;
+    let answer = 0;
+    let rest = 0;
+    while(n > 1 && n >= a){
+        rest = n % a
+        answer += parseInt(n / a) * b
+        n = parseInt(n / a) * b + rest
+    }
+    return answer
 }
