25f1f085d5b2c906ade95425499290338c873973
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2023-10-04 14:45:00 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -2,7 +2,6 @@ function solution(food) {
     var answer = '';
     let [left,right] = ['',''];
     for(let i in food){
-        if(i === 1) continue;
         if(food[i] / 2 > 0){
             left = left + i.toString().repeat(parseInt(food[i]/2))
             right = i.toString().repeat(parseInt(food[i]/2)) + right
