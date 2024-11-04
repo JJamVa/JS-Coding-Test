d3e7baafa318f3db9e852952fe87bae1810f2603
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2024-11-04 19:20:04 +0900

    [level 2] Title: 할인 행사, Time: 3073.83 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,11 +1,11 @@
 function solution(want, number, discount) {
     let day = 0;
-    for(let i = 0; i <= discount.length - 10; i++){
-        let copy_discount = [...discount].slice(i, i + 10);
+    while(discount.length >= 10){
+        let copy_discount = discount.slice(0, 10);
         let copy_number = [...number];
         
-        for(let j = 0; j < copy_discount.length; j++){
-            let value = copy_discount[j];
+        for(let i = 0; i < copy_discount.length; i++){
+            let value = copy_discount[i];
             let idx = want.findIndex((e) => e === value);
             if(idx !== -1 && copy_number[idx] > 0){
                 copy_number[idx]--;
@@ -14,6 +14,7 @@ function solution(want, number, discount) {
         if(copy_number.reduce((a,b) => a + b, 0) === 0) {
             day++;
         }
+        discount.shift();
     }
     
     return day;
