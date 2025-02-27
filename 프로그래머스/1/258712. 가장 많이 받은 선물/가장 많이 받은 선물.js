f94a7e29de0d1e86f16b60d2188f73ace0287559
Jun Hong Park <80045006+JJamVa@users.noreply.github.com>
2025-02-28 01:30:39 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 51.33 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -2,50 +2,52 @@ function solution(friends, gifts) {
     let user = new Map();
     let user_state = new Map();
     let next_gift = new Map();
-
-    // 1. 모든 친구를 초기화
-    for (let friend of friends) {
+    
+    for(let friend of friends){
         user.set(friend, new Map());
         user_state.set(friend, 0);
-        next_gift.set(friend, 0);
+        next_gift.set(friend, 0)
     }
-
-    // 2. 선물 기록 저장 및 선물 지수 계산
-    for (let gift of gifts) {
-        let [giver, receiver] = gift.split(" ");
-
-        // (1) 누가 누구에게 줬는지 기록
-        user.get(giver).set(receiver, (user.get(giver).get(receiver) || 0) + 1);
-
-        // (2) 선물 지수 업데이트 (준 선물 +1, 받은 선물 -1)
-        user_state.set(giver, user_state.get(giver) + 1);
-        user_state.set(receiver, user_state.get(receiver) - 1);
+    
+    // 누가 누구에게 줬는지 전체 gifts
+    while(gifts.length !== 0){
+        let value = gifts.shift().split(" ");
+        
+        if(!user.has(value[0])){
+            user.set(value[0], new Map());
+        }
+        
+        let info = user.get(value[0])
+        info.set(value[1], info.get(value[1]) ? info.get(value[1]) + 1 : 1);
+        user_state.set(value[0], user_state.get(value[0]) ? user_state.get(value[0]) + 1 : 1)
+        user_state.set(value[1], user_state.get(value[1]) ? user_state.get(value[1]) - 1 : - 1)
     }
-
-    // 3. 친구 쌍을 비교하여 추가 선물 계산
-    for (let i = 0; i < friends.length; i++) {
-        for (let j = i + 1; j < friends.length; j++) {
+    
+    // 선물 주고 받는거 조회 많이 받은애가 적게 준애한테 주기
+    for(let i = 0; i < friends.length; i++){
+        for(let j = i + 1; j < friends.length; j++){
             let friend_A = friends[i];
             let friend_B = friends[j];
-
-            let value1 = user.get(friend_A)?.get(friend_B) || 0;
-            let value2 = user.get(friend_B)?.get(friend_A) || 0;
-
-            if (value1 === value2) {
-                let gift_A = user_state.get(friend_A) || 0;
-                let gift_B = user_state.get(friend_B) || 0;
-
-                if (gift_A !== gift_B) {
-                    let receiver = gift_A > gift_B ? friend_A : friend_B;
-                    next_gift.set(receiver, next_gift.get(receiver) + 1);
+            
+            let value1 = user.get(friend_A)?.get(friend_B) || 0
+            let value2 = user.get(friend_B)?.get(friend_A) || 0
+            
+            if(value1 === value2){
+                let gift_A = user_state.get(friend_A);
+                let gift_B = user_state.get(friend_B);
+                
+                if(gift_A !== gift_B){
+                    gift_A > gift_B 
+                        ? next_gift.set(friend_A, next_gift.get(friend_A) ? next_gift.get(friend_A) + 1 : 1)
+                        : next_gift.set(friend_B, next_gift.get(friend_B) ? next_gift.get(friend_B) + 1 : 1)
                 }
-            } else {
-                let receiver = value1 > value2 ? friend_A : friend_B;
-                next_gift.set(receiver, next_gift.get(receiver) + 1);
+            }else if(value1 > value2){
+                next_gift.set(friend_A, next_gift.get(friend_A) ? next_gift.get(friend_A) + 1 : 1)
+            }else{
+                next_gift.set(friend_B, next_gift.get(friend_B) ? next_gift.get(friend_B) + 1 : 1)
             }
         }
     }
-
-    return Math.max(...next_gift.values());
+    
+    return next_gift.size ? Math.max(...next_gift.values()) : 0;
 }
-
